3a9079ccb46b1116774ac22b61db9c7a28558a03
Ravi Yadav <ravyyadav97@gmail.com>
2024-10-26 15:11:32 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+// import App from "./App.jsx";
 import "./index.css";
+import Appv from "./AppV2.jsx";
+
 import StarRating from "./StarRating.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+{/*     <App /> */}
+    <Appv />
   </React.StrictMode>
 );
 
